3cd3f74d03842becdfd591136b1f19faca577146
Aitor Morais <43671531+aitormorais@users.noreply.github.com>
2023-01-15 17:48:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,37 @@
-from telegram import Update
+import telegram
+from telegram import Update,KeyboardButton, ReplyKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler
 import threading
 import requests
+latitud,longitud=0,0
 
 def process_image(path):
 
     print("Processing image")
 
     ##### Process the image and detct dots here #####
-
+    print("introducidas:",latitud,longitud)
     placeholder_data = {
         "nombre":"Deusto",
-        "lat": "43.2730147", 
-        "lng": "-2.9559029",
+        "lat": latitud, 
+        "lng": longitud,
     }
 
     requests.post("http://127.0.0.1:8000/api/data/", json=placeholder_data)
 
 async def message_handler(update: Update, context):
+    #print("pedimos:")
+    #lat,lng=location_handler(update,context)
     # Check if there is any image attached
+
+    if update.message.location:
+        location = update.message.location
+        global latitud 
+        latitud = location.latitude
+        global longitud 
+        longitud = location.longitude
+        print("Imprimimos valores: ",latitud,longitud)
+
     if update.message.photo:
         print("Photo attached")
         # Get the file id of the image
@@ -29,7 +42,11 @@ async def message_handler(update: Update, context):
         file_url = f'https://api.telegram.org/file/bot{"5872441848:AAGuR3ed2YfBNjgD_OvBKERHxHO9sWhsG14"}/{file.file_path}'
         response = requests.get(file_url)
         open("image.jpg", "wb").write(response.content)
+        #location_handler(update,context)
         #await file.download("image.jpg")
+        #keyboard = [[telegram.KeyboardButton("Enviar mi ubicación", request_location=True)]]
+        #reply_markup = telegram.ReplyKeyboardMarkup(keyboard, one_time_keyboard=True)
+        #await update.message.reply_text("Image received. Please share your location.", reply_markup=reply_markup)
         # Send a message to the user
         t = threading.Thread(target=process_image, args=("image.jpg",))
         t.start()
