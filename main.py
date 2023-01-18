3e678858ed45ed726b520d0d750d46f91a82eefa
Aitor Morais <43671531+aitormorais@users.noreply.github.com>
2023-01-18 13:26:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from telegram import Update,KeyboardButton, ReplyKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler
 import threading
 import requests
-latitud,longitud=0,0
-
+latitud,longitud=181,181
+ubicacion = False
 def process_image(path):
 
     print("Processing image")
@@ -23,16 +23,17 @@ async def message_handler(update: Update, context):
     #print("pedimos:")
     #lat,lng=location_handler(update,context)
     # Check if there is any image attached
-
+    global ubicacion
     if update.message.location:
         location = update.message.location
         global latitud 
         latitud = location.latitude
         global longitud 
         longitud = location.longitude
+        ubicacion = True
         print("Imprimimos valores: ",latitud,longitud)
 
-    if update.message.photo:
+    if update.message.photo and ubicacion:
         print("Photo attached")
         # Get the file id of the image
         file_id = update.message.photo[-1].file_id
@@ -60,5 +61,6 @@ async def message_handler(update: Update, context):
 app = ApplicationBuilder().token("5872441848:AAGuR3ed2YfBNjgD_OvBKERHxHO9sWhsG14").build()
 
 #app.add_handler(CommandHandler("prompt", prompt))
+#app.bot.send_message(chat_id,"Por favor primero ingresar ubicacion y despues foto...")
 app.add_handler(MessageHandler(filters=None, callback=message_handler))
 app.run_polling()
