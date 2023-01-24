783816b3545678bd5b2e4cda2b44370c60432c10
Aitor Morais <43671531+aitormorais@users.noreply.github.com>
2023-01-24 19:12:28 +0100

    Update models.py

--- web/models.py
+++ web/models.py
@@ -5,6 +5,7 @@ class Ubicacion(models.Model):
     nombre = models.CharField(max_length=20)
     lat = models.CharField(max_length=200)
     lng = models.CharField(max_length=200)
+    contaminacion = models.CharField(max_length=200)
     fecha = models.DateTimeField(auto_now_add=True)
     def __unicode__(self):
         return self.nombre
