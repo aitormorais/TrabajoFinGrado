7374a5ea4410324716772d7d8bb1a0f29854d38c
Aitor Morais <aitormorais@opendeusto.es>
2023-02-06 01:02:29 +0100

    actualizado

--- bot/bot.py
+++ bot/bot.py
@@ -12,37 +12,37 @@ import urllib
 import json
 import re 
 import tratar_imagen as t_img
-logging.basicConfig(level=logging.ERROR)
-latitud,longitud=0,0
 import logging
-logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+latitud,longitud=0,0
+#logging.basicConfig(level=logging.ERROR)
+context = {}
+logging.basicConfig(level=logging.DEBUG, filename='app.log', filemode='w', format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
 async def process_image(update, context):
     try:
         logging.info("Procesando imagen")
         contamina = t_img.tratar("a.jpg")
-        print("COTAMIACIODTCTADA: ",contamina)
-        print("introducidas:",latitud,longitud)
         peticion = "https://maps.googleapis.com/maps/api/geocode/json?latlng={},{}&key=AIzaSyAk-jcafbBJsvLF2MnZS9m0wOgQCDi4mCs".format(latitud,longitud)
         response = requests.get(peticion)
         if response.status_code == 200:
             online = response.json()
             json_data = json.dumps(online)
-            print(type(json_data))
             data = response.json()
             # Guarda el JSON en un archivo
-            with open("data.json", "w") as outfile:
-                json.dump(data, outfile)
+            with open("data.json", "w") as file:
+                json.dump(data, file)
             with open('data.json', 'r') as file:
                 data = json.load(file)
             r = data['results']
-            texto =str(r[0])
-            result = re.search(r"'formatted_address': '(.*?)'", texto)
-            nombre = ""
-            if result:
-                nombre = result.group(1)
+            if r:
+                texto =str(r[0])
+                result = re.search(r"'formatted_address': '(.*?)'", texto)
+                nombre = ""
+                if result:
+                    nombre = result.group(1)
+                else:
+                    nombre = "ubicacion desconocida"
             else:
                 nombre = "ubicacion desconocida"
-            
         else:
             print("La petición GET falló con el código de estado:", response.status_code)
         placeholder_data = {
@@ -51,11 +51,15 @@ async def process_image(update, context):
             "lng": longitud,
             "contaminacion":contamina,
         }
-        requests.post("https://aitorm.pythonanywhere.com/api/data/", json=placeholder_data)
+        if latitud == 0 and longitud == 0:
+            await update.message.reply_text("Por favor, envía tus coordenadas antes de enviar la imagen.")
+            return
+        requests.post("http://127.0.0.1:8000/api/data/", json=placeholder_data)
         await asyncio.sleep(1)
     except Exception as e:
         logging.error(f"Ha ocurrido un error: {e}")
-        await context.bot.send_message(chat_id=update.effective_chat.id, text="Lo siento, ha ocurrido un error inesperado. Por favor, inténtalo de nuevo más tarde.")
+        await context.bot.send_message(chat_id=update.message.chat_id, text="Lo siento, ha ocurrido un error inesperado. Por favor, inténtalo de nuevo más tarde...")
+        #await context.bot.send_message(chat_id=update.effective_chat.id, text="Lo siento, ha ocurrido un error inesperado. Por favor, inténtalo de nuevo más tarde.")
 async def message_handler(update: Update, context):
     if update.message.location:
         location = update.message.location
@@ -63,21 +67,17 @@ async def message_handler(update: Update, context):
         latitud = location.latitude
         global longitud 
         longitud = location.longitude
-
     if update.message.photo:
-
         file_id = update.message.photo[-1].file_id
         foto = await context.bot.get_file(file_id)
         response = requests.get(foto.file_path)
         with open('a.jpg', 'wb') as f:
             f.write(response.content)
-        await update.message.reply_text("Image received. Processing...")
+        await update.message.reply_text("Imagen recibida, Procesando...")
         await process_image(Update, context)
         await context.bot.send_message(chat_id=update.effective_chat.id, text="Gracias, hemos procesado su imagen.")     
-  
     else:
-        await update.message.reply_text("No image attached")
-
+        await update.message.reply_text("No se a subido imagen")
 app = ApplicationBuilder().token("5872441848:AAGuR3ed2YfBNjgD_OvBKERHxHO9sWhsG14").build()
 app.add_handler(MessageHandler(filters=None, callback=message_handler))
 app.run_polling()
